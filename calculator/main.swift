c2551728257514fddf52dfdb36fa65d5efcffb04
CT4TuEI3 <alexstatiev@gmail.com>
2022-10-08 14:29:33 +0300

    v3.0 New Functionality
    1. Добавленна возможность "выхода" из программы при помощи горячей клавиши.
    2. Добавленна возможность просмотра история вычислений
    3. После вычисления примера программа готова сразу к вычислению следующего примера

--- calculator/main.swift
+++ calculator/main.swift
@@ -5,27 +5,50 @@
 //  Created by Алексей on 16.09.2022.
 //
 
-//import Foundation
+import Darwin
 
 print("Добро пожаловать в калькулятор")
 
-let operation = getDataFromUser(description: "Выберите операцию: +, -, * или /")
-let firstNumber = getDataFromUser(description: "Введите первое число")
-let secondNumber = getDataFromUser(description: "Введите второе числов")
+var history: [String] = []
 
-print("Идёт вычисление: " + firstNumber + " " + operation + " " + secondNumber)
-
-let result = "Результат вычисления: "
-if let firstNumber = Int(firstNumber) {
-    if let secondNumber = Int(secondNumber) {
-        calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
+while true {
+    let operation = getDataFromUser(description: "Выберите операцию: +, -, * или /. Для завершения работы введите q. Для просмотра истории введите h")
+    if operation == "q" {
+        exit(0)
+    } else if operation == "h" {
+        for example in history {
+            print(example)
+        }
+        continue
+    }
+    let firstNumber = getDataFromUser(description: "Введите первое число")
+    let secondNumber = getDataFromUser(description: "Введите второе числов")
+    
+    let example = firstNumber + " " + operation + " " + secondNumber
+    print("Идёт вычисление: " + firstNumber + " " + operation + " " + secondNumber)
+    
+    let _ = "Результат вычисления: "
+    if let firstNumber = Int(firstNumber) {
+        if let secondNumber = Int(secondNumber) {
+            let result = calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
+            if let result = result {
+                showResult(result)
+                history.append(example + " = " + String(result))
+            }
+        }
+        
+    } else {
+        print("Вы ввели неверное первое число")
     }
     
-} else {
-    print("Вы ввели неверное первое число")
+    print("")
+    print("--------------------------------")
+    print("")
+
 }
 
 func getDataFromUser(description: String) -> String {
+    print(description)
     return readLine() ?? ""
 }
 
@@ -35,22 +58,22 @@ func showResult(_ result: Int) {
     print(description + " " + result)
 }
 
-func calculate(operation: String, firstNumber: Int, secondNumber: Int) {
+func calculate(operation: String, firstNumber: Int, secondNumber: Int) -> Int?{
     switch operation {
-    case "+": showResult(firstNumber + secondNumber)
+    case "+": return firstNumber + secondNumber
         
-    case "-": showResult(firstNumber - secondNumber)
+    case "-": return firstNumber - secondNumber
         
-    case "*": showResult(firstNumber * secondNumber)
+    case "*": return firstNumber * secondNumber
         
-    case "/": secondNumber != 0 ? showResult(firstNumber / secondNumber) : print("На 0 делить нельзя!")
-        
+    case "/":
+        if secondNumber != 0 {
+            return firstNumber / secondNumber
+        } else {
+            print("На 0 делить нельзя!")
+            return nil
+        }
     default: print("Вы ввели неверную операцию")
+        return nil
     }
 }
-
-//test
-
-//test2
-
-
