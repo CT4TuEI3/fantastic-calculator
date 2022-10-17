0f86c96315bc6dc95f932fe2536bf58db0279fe6
CT4TuEI3 <alexstatiev@gmail.com>
2022-10-17 19:12:03 +0300

    v4.0 Refactoring program with guard
    Рефакторинг калькулятора при помощи оператор guard. Исправлена проблема большой вложенности в конструкции switch.

--- calculator/main.swift
+++ calculator/main.swift
@@ -1,10 +1,3 @@
-//
-//  main.swift
-//  calculator
-//
-//  Created by Алексей on 16.09.2022.
-//
-
 import Darwin
 
 print("Добро пожаловать в калькулятор")
@@ -12,39 +5,52 @@ print("Добро пожаловать в калькулятор")
 var historyOfCalculation: [String] = []
 
 while true {
-    let operation = getDataFromUser(description: "Выберите операцию: +, -, * или /. Для завершения работы введите q. Для просмотра истории введите h")
-    if operation == "q" {
+    let action = getDataFromUser(description: "Нажмите: С - для рассчёта примера. Q - для завершения работы. H - просмотр истории")
+    
+    switch action {
+    case "c":
+        calculate()
+    case "q":
         exit(0)
-    } else if operation == "h" {
-        for example in historyOfCalculation{
-            print(example)
-        }
-        continue
-    }
-    let firstNumber = getDataFromUser(description: "Введите первое число")
-    let secondNumber = getDataFromUser(description: "Введите второе числов")
-    
-    let example = firstNumber + " " + operation + " " + secondNumber
-    print("Идёт вычисление: " + firstNumber + " " + operation + " " + secondNumber)
-    
-    let _ = "Результат вычисления: "
-    if let firstNumber = Int(firstNumber) {
-        if let secondNumber = Int(secondNumber) {
-            let result = calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
-            if let result = result {
-                showResult(result)
-                historyOfCalculation.append(example + " = " + String(result))
-            }
-        }
-    } else {
-        print("Вы ввели неверное первое число")
+    case "h":
+        showHistory()
+    default:
+        print("Недопустимое действие")
     }
     
     print("")
     print("--------------------------------")
     print("")
-
 }
+
+func calculate() {
+    let operation = getDataFromUser(description: "Выберите операцию: +, -, * или /.")
+    guard operation == "+" || operation == "-" || operation == "*" || operation == "/" else { return
+        print("Вы ввели невверную операцию.")
+    }
+    
+    let firstNumber = getDataFromUser(description: "Введите первое число")
+    guard let firstNumber = Int(firstNumber) else {
+        print("Вы ввели неверное число")
+        return
+    }
+    
+    let secondNumber = getDataFromUser(description: "Введите второе числов")
+    guard let secondNumber = Int(secondNumber) else {
+        print("Вы ввели неверное число")
+        return
+    }
+    
+    let example = String(firstNumber) + " " + operation + " " + String(secondNumber)
+    print("Идёт вычисление: " + example)
+    
+    let result = calculate(operation: operation, firstNumber: firstNumber, secondNumber: secondNumber)
+    guard let result = result else { return }
+    
+    showResult(result)
+    historyOfCalculation.append(example + " = " + String(result))
+}
+
 
 func getDataFromUser(description: String) -> String {
     print(description)
@@ -59,20 +65,28 @@ func showResult(_ result: Int) {
 
 func calculate(operation: String, firstNumber: Int, secondNumber: Int) -> Int?{
     switch operation {
-    case "+": return firstNumber + secondNumber
+    case "+":
+        return firstNumber + secondNumber
         
-    case "-": return firstNumber - secondNumber
+    case "-":
+        return firstNumber - secondNumber
         
-    case "*": return firstNumber * secondNumber
+    case "*":
+        return firstNumber * secondNumber
         
-    case "/":
-        if secondNumber != 0 {
-            return firstNumber / secondNumber
-        } else {
-            print("На 0 делить нельзя!")
-            return nil
-        }
-    default: print("Вы ввели неверную операцию")
+    case "/" where secondNumber == 0:
+        print("На 0 делить нельзя")
         return nil
+    case "/":
+        return firstNumber / secondNumber
+    default:
+        print("Вы ввели неверную операцию")
+        return nil
+    }
+}
+
+func showHistory() {
+    for example in historyOfCalculation {
+        print(example)
     }
 }
